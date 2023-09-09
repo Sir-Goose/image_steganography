4d4764c420a208c273104598bb0c3a06ed777d93
Matthew Allison <matthewallisonxyz@gmail.com>
2023-09-09 09:23:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 def convert_text_to_binary(text_in):
-    return text_in.encode('utf-8')
 
+
+def convert_binary_to_text(binary_in):
+
+def binaray_addition(binary_in):
+
+
+
+
+
+
+if __name__ == '__main__':
+    text = 'Hello, world!'
+    binary = convert_text_to_binary(text)
+
+    for character in binary:
+        print(character)
 
 
 
