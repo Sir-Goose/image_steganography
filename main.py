be79a85ac5b81c4ea20e3b39a5927c13818a7f0b
Matthew Allison <matthewallisonxyz@gmail.com>
2023-09-10 14:37:40 +0200

    abandoning project

--- main.py
+++ main.py
@@ -19,6 +19,34 @@ def binaray_addition(binary_in):
     return
 
 
+def convert_image_to_array(image_in):
+    # Open the image using PIL
+    img = Image.open(image_in)
+
+    # Convert image to RGB (in case it's in another format like RGBA or Grayscale)
+    img = img.convert("RGB")
+
+    # Get image dimensions
+    width, height = img.size
+
+    # Create a 2D array to store the RGB values of each pixel
+    rgb_array_2d = []
+
+    # Loop through each row
+    for y in range(height):
+        row = []
+        for x in range(width):
+            # Get RGB values of each pixel
+            r, g, b = img.getpixel((x, y))
+
+            # Append the RGB tuple to the row
+            row.append((r, g, b))
+
+        # Append the row to the 2D array
+        rgb_array_2d.append(row)
+
+    return rgb_array_2d
+
 def check_sufficient_capacity(image_in, text_in):
     # Open the image using PIL
     img = Image.open(image_in)
@@ -36,6 +64,31 @@ def check_sufficient_capacity(image_in, text_in):
     return image_capacity >= binary_size
 
 
+def encode_text_in_image(image_array_in, text_in):
+    if check_sufficient_capacity(image_array_in, text_in):
+        print('Sufficient capacity')
+    else:
+        print('Insufficient capacity')
+        return
+
+    bit_array = []
+    binary_text = convert_text_to_binary(text_in)
+    for character in binary_text:
+        for bit in character:
+            bit_array.append(bit)
+
+    i = 0
+    for row in image_array_in:
+        for pixel in row:
+            for color in pixel:
+                print(color)
+
+
+
+    return
+
+
+
 if __name__ == '__main__':
     text = 'Hello, world!'
     binary = convert_text_to_binary(text)
@@ -45,3 +98,7 @@ if __name__ == '__main__':
     print(binary)
 
     print(check_sufficient_capacity('elon.jpeg', text))
+    encode_text_in_image(convert_image_to_array('elon.jpeg'), text)
+
+
+
