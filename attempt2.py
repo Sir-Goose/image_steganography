b64f9ddd403cf63e1a168756cba6b38c8a8a1e21
Matthew Allison <matthewallisonxyz@gmail.com>
2023-09-11 21:43:48 +0200

    Decoding and encoding complete
    Has bugs, doesn't work properly.
    Will clean up the code and then fix.

--- attempt2.py
+++ attempt2.py
@@ -30,10 +30,34 @@ def convert_image_to_array(image_in):
     return rgb_array_2d
 
 
+def convert_array_to_image(rgb_array_2d):
+    # Determine the dimensions of the 2D array
+    height = len(rgb_array_2d)
+    width = len(rgb_array_2d[0])
+
+    # Create a new blank image with the same dimensions
+    img_new = Image.new("RGB", (width, height))
+
+    # Populate the new image with the RGB values from the 2D array
+    for y in range(height):
+        for x in range(width):
+            # Get the RGB values from the 2D array
+            r, g, b = rgb_array_2d[y][x]
+
+            # Set the RGB values for each pixel in the new image
+            img_new.putpixel((x, y), (r, g, b))
+
+    return img_new
+
 
 def convert_decimal_to_binary(decimal_in):
     binary_out = format(decimal_in, '08b')
     return binary_out
+
+
+def convert_binary_to_decimal(binary_in):
+    decimal_out = int(binary_in, 2)
+    return decimal_out
 
 
 def convert_decimal_array_to_binary(image_array):
@@ -43,6 +67,18 @@ def convert_decimal_array_to_binary(image_array):
             pixel[0] = convert_decimal_to_binary(pixel[0])
             pixel[1] = convert_decimal_to_binary(pixel[1])
             pixel[2] = convert_decimal_to_binary(pixel[2])
+            image_array[i][j] = tuple(pixel)
+
+    return image_array
+
+
+def convert_binary_array_to_decimal(image_array):
+    for i in range(len(image_array)):
+        for j in range(len(image_array[i])):
+            pixel = list(image_array[i][j])
+            pixel[0] = convert_binary_to_decimal(pixel[0])
+            pixel[1] = convert_binary_to_decimal(pixel[1])
+            pixel[2] = convert_binary_to_decimal(pixel[2])
             image_array[i][j] = tuple(pixel)
 
     return image_array
@@ -96,14 +132,94 @@ def adjust_least_significant_bit(image_array, binary_string):
 
     return image_array
 
+def get_least_significant_bits(image_array):
+    decoded_bits = []
+    for i in range(len(image_array)):
+        for j in range(len(image_array[i])):
+            pixel = list(image_array[i][j])
+            decoded_bits.append(pixel[0][len(pixel[0]) - 1])
+            decoded_bits.append(pixel[1][len(pixel[1]) - 1])
+            decoded_bits.append(pixel[2][len(pixel[2]) - 1])
+    return decoded_bits
+
+def convert_8_bit_binary_to_character(binary_string):
+    character = chr(int(binary_string, 2))
+    return character
 
 
-rgb_array_2d_decimal = convert_image_to_array('elon.jpeg')
-rgb_array_2d_binary = convert_decimal_array_to_binary(rgb_array_2d_decimal)
-print(rgb_array_2d_binary)
 
-binary_string = convert_string_to_binary_string('Hello, world!')
-print(binary_string)
+def encode_text_in_image():
+    rgb_array_2d_decimal = convert_image_to_array('elon.jpeg')
+    rgb_array_2d_binary = convert_decimal_array_to_binary(rgb_array_2d_decimal)
+    print(rgb_array_2d_binary)
 
-rgb_array_2d_binary = adjust_least_significant_bit(rgb_array_2d_binary, binary_string)
-print(rgb_array_2d_binary)
+    # Open the file for reading
+    with open('hamlet.txt', 'r') as file:
+        content = file.read()
+
+    print(content)  # This will print the contents of the file
+
+    binary_string = convert_string_to_binary_string(content)
+    print(binary_string)
+
+    rgb_array_2d_binary = adjust_least_significant_bit(rgb_array_2d_binary, binary_string)
+    print(rgb_array_2d_binary)
+
+    # convert image back to decimal array
+    rgb_array_2d_decimal = convert_binary_array_to_decimal(rgb_array_2d_binary)
+    # print(rgb_array_2d_decimal)
+
+    # convert decimal array to image
+    img_new = convert_array_to_image(rgb_array_2d_decimal)
+    img_new.save('test.png')
+
+def decode_text_from_image():
+    print("decoding...")
+    rgb_array_2d_decimal = convert_image_to_array('test.png')
+    rgb_array_2d_binary = convert_decimal_array_to_binary(rgb_array_2d_decimal)
+    decoded_bits = get_least_significant_bits(rgb_array_2d_binary)
+
+    binary_characters = []
+    for bit in range(len(decoded_bits)):
+        if bit % 8 == 0:
+            binary_characters.append(decoded_bits[bit:bit+8])
+
+
+    text_characters = []
+    for character in binary_characters:
+        text_characters.append(convert_8_bit_binary_to_character(''.join(character)))
+
+    # print(text_characters)
+
+    with open('output.txt', 'w') as f:
+        f.write(''.join(text_characters))
+
+
+
+
+
+
+    # read LSB of each color value of each pixel and add to array of arrays of 8 bits each
+    # one array of 8 bits for each character
+
+encode_text_in_image()
+decode_text_from_image()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
